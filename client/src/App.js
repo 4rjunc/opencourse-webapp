e8b7106f44bf9fadfa4aecbfc1dec94b41fc3454
H4K3R13 <arjunmundamani@gmail.com>
2023-04-20 12:23:33 +0530

    Login and register Clean Up

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,54 @@
 import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 function App() {
   return (
     <Router>
-          
-            <Link  to={'/login'}>
-              OpenCourse
-            </Link>  
-            <Link  to={'/login'}>
+          <List>  
+            <SLink  to={'/login'}>
                Login
-            </Link>  
-            <Link  to={'/register'}>
+            </SLink>  
+            <SLink  to={'/register'}>
                 Register
-            </Link>
+            </SLink>
+          </List>
             <Routes>
               <Route exact path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-            </Routes>
-         
+            </Routes>     
     </Router>
   )
 }
 export default App
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+    @media only screen and (max-width: 768px) {
+        flex-wrap: wrap;
+    }
+`;
+
+const SLink = styled(NavLink)`
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+    border-radius:10%;
+    margin-right: 3rem;
+    text-decoration: none;
+    background-color:grey;
+    width: 7rem;
+    height: 3rem;
+    cursor: pointer;  
+
+    @media only screen and (max-width: 768px) {
+        margin-right: 1rem;
+        margin-bottom: 1rem;
+    }
+`;

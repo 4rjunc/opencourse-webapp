d8556007900dbf507c7ac310a12e87b2ab408632
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-08-14 11:40:41 +0530

    404 design issue

--- openFrontend/src/components/NotFound.jsx
+++ openFrontend/src/components/NotFound.jsx
@@ -3,8 +3,15 @@ import { Box } from '@mui/material'
 function NotFound() {
   return (
     <>
-    <Box>
-        404 Not Found
+    <Box
+     sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: "center",
+        minHeight: '100vh',
+        fontSize:"2.5rem"
+     }}>
+       <p>404 Not Found</p>
     </Box>
     </>
   )

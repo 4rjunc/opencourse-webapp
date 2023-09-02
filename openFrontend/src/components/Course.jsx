ac27b3f4e2895de1c063c770484e3224b756d0e3
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-02 23:22:48 +0530

    ⭐️Alignment fix

--- openFrontend/src/components/Course.jsx
+++ openFrontend/src/components/Course.jsx
@@ -193,8 +193,8 @@ const Course = () => {
                   const choice = index + 1;
                   const selectedCourseKey = getCourseCodeForKey(choice);
                   return (
-                    <li key={index} style={{ marginTop: 10, fontSize:"1.3rem", listStyleType:"none"}}>
-                      {index + 1}{" "}
+                    <li key={index} style={{ marginTop: 10, fontSize:"1.3rem", listStyleType:"none",  display: "flex", alignItems: "center", fontWeight:"700"}}>
+                      {index + 1}:{"   "}
                       <FormControl variant="outlined" sx={{ minWidth: 300 }}>
                         <InputLabel>
                           {selectedCourseKey ? (

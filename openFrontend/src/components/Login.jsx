dcf9d42addc52c7593706fcac728899119cd01a0
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-04 23:25:10 +0530

    👍🏼Minor Changes

--- openFrontend/src/components/Login.jsx
+++ openFrontend/src/components/Login.jsx
@@ -14,7 +14,6 @@ import Box from "@mui/material/Box";
 import SchoolIcon from "@mui/icons-material/School";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";

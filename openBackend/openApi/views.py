bc6a0c277d98f843ca185cbb17e09d5fc0ed0119
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-05 14:52:02 +0530

    ⭐️Added Loading for Downloads

--- openBackend/openApi/views.py
+++ openBackend/openApi/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from django.db.models import Q
 from django.core import serializers
 import csv
+import pandas as pd
 
 
 
@@ -236,4 +237,38 @@ def get_submissions(request):
         #     return JsonResponse({'message': 'No submissions found'}, status=404)
 
 def allotement(request):
-    return JsonResponse({"message":"Success"}, status=200)
+        print("hello")
+        opencourse = Course.objects.filter(Q(course_type=2) & Q(syllabus_intro_year=2019))
+        courses_list = [course.course_code for course in opencourse]
+        header = ["Name", "Class","Reg No.", "Marks"]
+        header += courses_list
+       
+        student_data = {}  # Create a dictionary to store data for each student
+
+        submissions = OpenCourseChoice.objects.all()
+        for sub in submissions:
+            student_id = sub.stud_id.uty_reg_no
+            if student_id not in student_data:
+                student_data[student_id] = {
+                    "Name": sub.stud_id.name,
+                    "Class": sub.stud_id.pgm,
+                    "Reg No.": sub.stud_id.uty_reg_no,
+                    "Marks": sub.stud_id.marks_twelth,
+                }
+                # Initialize choices for all courses with 99 (or another default value)
+                student_data[student_id].update({course: 99 for course in courses_list})
+
+            # Update the choice for the specific course
+            student_data[student_id][sub.course_code] = sub.choice
+
+        data = pd.DataFrame(student_data)
+        data = data.T
+        print(f"{data = }")
+        #data.to_csv('allote.csv', index=False)
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="allote.csv"'
+
+        # Write the DataFrame to the response
+        data.to_csv(response, index=False)
+
+        return response

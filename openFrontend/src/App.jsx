4d0ec8344b4611624315255682c4ea2861608bdf
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-11 19:33:39 +0530

    ⭐️Animations

--- openFrontend/src/App.jsx
+++ openFrontend/src/App.jsx
@@ -1,36 +1,49 @@
-import './index.css'
+import "./index.css";
 //import Navbar from './pages/Navbar'
-import Course from './components/Course'
-import Login from './components/Login'
-import { Routes, Route, BrowserRouter} from 'react-router-dom'
-import NotFound from './components/NotFound'
-import PrivateRoute from './components/PrivateRoute'
-import Administrator from './components/Administrator'
-import Footer from './components/Footer'
-
+import Course from "./components/Course";
+import Login from "./components/Login";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import PrivateRoute from "./components/PrivateRoute";
+import Administrator from "./components/Administrator";
+import Footer from "./components/Footer";
+import { motion } from "framer-motion";
 
 function App() {
-  return (
+  return ( 
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/course' element={
-        <PrivateRoute>
-           <Course/> 
-        </PrivateRoute>
-        }/> 
-        <Route path='/' element={<Login/>}/>
-        <Route path='*' element={<NotFound/>}/>
-        <Route path='/administrator' element={
-          <PrivateRoute>
-            <Administrator/>
-          </PrivateRoute>
-        }/>
-      </Routes> 
-    </BrowserRouter>
-    <Footer/>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/course"
+            element={
+              <PrivateRoute>
+                <motion.div
+                  initial={{ opacity: 0, y: -20 }} // Initial state
+                  animate={{ opacity: 1, y: 0 }} // Animate to this state
+                  exit={{ opacity: 0, y: -20 }} // Exit animation
+                  transition={{ duration: 0.5 }} // Animation duration
+                >
+                  <Course />
+                </motion.div>
+              </PrivateRoute>
+            }
+          />
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+          <Route
+            path="/administrator"
+            element={
+              <PrivateRoute>
+                <Administrator />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

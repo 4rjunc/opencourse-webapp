b1de713c0d69622d1f2c516b41fc9d4fff9d3a92
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-16 17:44:26 +0530

    Made the text box green 🚀

--- openFrontend/src/components/Login.jsx
+++ openFrontend/src/components/Login.jsx
@@ -4,11 +4,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-import LoginIcon from '@mui/icons-material/Login';
+import LoginIcon from "@mui/icons-material/Login";
 //import FormControlLabel from '@mui/material/FormControlLabel';
 //import Checkbox from '@mui/material/Checkbox';
 //import Link from '@mui/material/Link';
 //import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
@@ -20,6 +21,26 @@ import IconButton from "@mui/material/IconButton";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
+
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#228B22',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#228B22',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'grey',
+    },
+    '&:hover fieldset': {
+      borderColor: 'black',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#228B22',
+    },
+  },
+});
 
 const Login = () => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
@@ -33,24 +54,24 @@ const Login = () => {
     setSnackbarOpen(false);
   };
   const footerStyle = {
-    backgroundColor: '#228B22',
-    width: '100%',
-    position: 'fixed',
+    backgroundColor: "#228B22",
+    width: "100%",
+    position: "fixed",
     bottom: 0,
     left: 0,
     zIndex: 999,
-    transition: 'bottom 0.3s', // Add a smooth transition for the footer
+    transition: "bottom 0.3s", // Add a smooth transition for the footer
   };
-  
+
   const contentStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   };
-  
+
   const textStyle = {
-    marginBottom: '6px',
+    marginBottom: "6px",
   };
 
   const handleSubmit = async (event) => {
@@ -68,23 +89,23 @@ const Login = () => {
       );
       const token = response.data["session_token"];
       const staff = response.data["staff"];
-      console.log("Login Response",response.data);
+      console.log("Login Response", response.data);
       localStorage.setItem("token", token);
       const regno = encodeURIComponent(username); // URL-encode the email
-      let url
+      let url;
       if (!staff) {
         url = `/course/?regno=${regno}`;
-      } else if (staff) { // Use regexadmin.test() here
+      } else if (staff) {
+        // Use regexadmin.test() here
         url = `/administrator`; // Correct the typo in the URL path as well
       }
       setSnackbarOpen(true);
       setSnackbarMessage("Login successful!");
       setSnackbarSeverity("success");
 
-      setTimeout(()=>{
+      setTimeout(() => {
         navigate(url);
-      }, 1500)
-      
+      }, 1500);
     } catch (error) {
       console.error("Login failed:", error);
       setSnackbarOpen(true);
@@ -93,23 +114,30 @@ const Login = () => {
     }
   };
 
-
-
   return (
-
-    <div style={{ background: 'whiteSmoke', height: '100vh', overflow: 'hidden'}}>
-      <div style={{marginBottom: "3.5rem", marginTop: "2rem", marginRight:"60%"  }}>
+    <div
+      style={{ background: "whiteSmoke", height: "100vh", overflow: "hidden" }}
+    >
+      <div
+        style={{
+          marginBottom: "3.5rem",
+          marginTop: "2rem",
+          marginRight: "60%",
+        }}
+      >
         <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="fixed" 
-            sx={{  
-              backgroundColor: "white", 
-              color:"black", 
-              borderRadius:"1rem", 
+          <AppBar
+            position="fixed"
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              borderRadius: "1rem",
               marginTop: ".5rem",
               marginLeft: ".3rem",
-              marginRight: ".3rem", 
-              width:"99%", 
-              boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)"}}
+              marginRight: ".3rem",
+              width: "99%",
+              boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)",
+            }}
           >
             <Toolbar>
               <IconButton
@@ -119,7 +147,11 @@ const Login = () => {
                 aria-label="menu"
                 sx={{ mr: 2 }}
               >
-                <img src={logo} alt="Logo" style={{ width: '50px', height: '50px', marginLeft: '8px' }} />
+                <img
+                  src={logo}
+                  alt="Logo"
+                  style={{ width: "50px", height: "50px", marginLeft: "8px" }}
+                />
               </IconButton>
               <Typography
                 variant="h4"
@@ -130,17 +162,17 @@ const Login = () => {
               </Typography>
             </Toolbar>
           </AppBar>
-        </Box>     
+        </Box>
         <Container component="main" maxWidth="xs">
           <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
-          ></div> 
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          ></div>
 
           <CssBaseline />
           <Box
@@ -150,77 +182,72 @@ const Login = () => {
               flexDirection: "column",
               alignItems: "center",
             }}
-          >    
+          >
             <Box
               component="form"
               onSubmit={handleSubmit}
               noValidate
               sx={{ mt: 15 }}
             >
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="regno"
-                  label="Register Number"
-                  name="regno"
-                  onChange={(e) => setUsername(e.target.value)}
-                  autoFocus
-                  InputProps={{
-                    style: { borderColor: '#27ac1f' }, // Set border color to green
-                    focused: { borderColor: '#27ac1f' } // Set focused border color to green
-                  }}
-                  InputLabelProps={{
-                    style: { color: 'black' }, // Set label color to black
-                  }}  
-                /> 
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  placeholder="YYYY-MM-DD"
-                  autoComplete="current-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  InputLabelProps={{
-                    style: { color: 'black' }, // Set label color to black
-                  }}  
-                />
-                {/*<FormControlLabel
+              <CssTextField
+                margin="normal"
+                required
+                fullWidth
+                id="regno"
+                label="Register Number"
+                name="regno"
+                onChange={(e) => setUsername(e.target.value)}
+                autoFocus
+              />
+              <CssTextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                placeholder="YYYY-MM-DD"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {/*<FormControlLabel
                   control={<Checkbox value="remember" color="primary" />}
                   label="Remember me"
                 />*/}
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  <Button
-                    type="submit"
-                    //fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2, background:"#27ac1f !important", borderRadius:"2rem" }}
-                    onClick={handleSubmit}
-                    startIcon={<LoginIcon/>}
-                  >
-                    Login
-                  </Button>
-                  </Box>
-              <Box sx={{textAlign:"center"}}>
-              <Snackbar
-                open={snackbarOpen}
-                autoHideDuration={5000}
-                onClose={handleSnackbarClose}
-                //style={{padding:"20rem 31.2rem"}}
-              >
-                <MuiAlert
-                  elevation={6}
-                  variant="filled"
-                  onClose={handleSnackbarClose}
-                  severity={snackbarSeverity}
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button
+                  type="submit"
+                  //fullWidth
+                  variant="contained"
+                  sx={{
+                    mt: 3,
+                    mb: 2,
+                    background: "#27ac1f !important",
+                    borderRadius: "2rem",
+                  }}
+                  onClick={handleSubmit}
+                  startIcon={<LoginIcon />}
                 >
-                  {snackbarMessage}
-                </MuiAlert>
-              </Snackbar>
+                  Login
+                </Button>
+              </Box>
+              <Box sx={{ textAlign: "center" }}>
+                <Snackbar
+                  open={snackbarOpen}
+                  autoHideDuration={5000}
+                  onClose={handleSnackbarClose}
+                  //style={{padding:"20rem 31.2rem"}}
+                >
+                  <MuiAlert
+                    elevation={6}
+                    variant="filled"
+                    onClose={handleSnackbarClose}
+                    severity={snackbarSeverity}
+                  >
+                    {snackbarMessage}
+                  </MuiAlert>
+                </Snackbar>
               </Box>
               {/* <Grid container>
                 <Grid item xs>
@@ -235,18 +262,18 @@ const Login = () => {
                 </Grid>
               </Grid>*/}
             </Box>
-          </Box>  
+          </Box>
         </Container>
       </div>
       <AppBar position="static" style={footerStyle}>
-          <Container>
-            <Toolbar style={contentStyle}>
-              <Typography variant="body1" color="white" style={textStyle}>
-                Nehru Arts and Science College Padannakad
-              </Typography>
-            </Toolbar>
-          </Container>
-      </AppBar> 
+        <Container>
+          <Toolbar style={contentStyle}>
+            <Typography variant="body1" color="white" style={textStyle}>
+              Nehru Arts and Science College Padannakad
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </div>
   );
 };

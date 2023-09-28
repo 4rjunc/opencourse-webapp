3fe363d0491d48d3637cd9bd5718b7c89234737b
GOPIKAKRISHNANR <gopikakrishnanr2002@gmail.com>
2023-09-28 20:44:18 +0530

    marginTop adjusted

--- openFrontend/src/components/Login.jsx
+++ openFrontend/src/components/Login.jsx
@@ -154,7 +154,7 @@ const Login = () => {
           <CssBaseline />
           <Box
             sx={{
-              marginTop: 8,
+              marginTop: 11.7,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
